a440bb20bbaacfaf3672eb31022e216e5e277d6a
Guilherme Macedo <guilhermecedoanastacio@gmail.com>
2024-09-28 01:22:34 -0300

    Refinamentos

--- src/controller/HouseController.js
+++ src/controller/HouseController.js
@@ -2,41 +2,43 @@ import Houses from "../models/Houses";
 
 class HouseController {
   async store(req, res) {
-    try{
+    try {
       const file = req.file;
-    const { house, valor, status } = req.body;
-    const { user_id } = req.headers;
+      const { house, valor, status } = req.body;
+      const { user_id } = req.headers;
+      const casa = await Houses.findOne({ house })
+      if(casa != null){
+        if (await Houses.find({ house })) {
+          return res.status(400).json({
+            msg: "Ja existe essa casa",
+          });
+        }
+      }
+      if (!user_id) {
+       return res.status(401).json({
+          msg: "Não autorizado",
+        });
+      }
+      if (!house || !valor || !status) {
+       return res.status(400).json({
+          msg: "Favor preencher todos os campos",
+        });
+      }
 
+      const Casa = await Houses.create({
+        image: file.filename,
+        user: user_id,
+        house,
+        valor,
+        status,
+      });
 
-    if( !house || !valor || !status ){
-        res.status(400).json({
-            msg : "Favor preencher todos os campos"
-        })
-    }
-    if (!user_id) {
-      res.status(401).json({
-        msg: "Não autorizado",
+      return res.status(200).json(Casa);
+    } catch (e) {
+      return res.status(400).json({
+        msg: e.message,
       });
     }
-    
-    const Casa = await Houses.create({
-      image: file.filename,
-      user: user_id,
-      house,
-      valor,
-      status,
-    });
-    
-    return res.status(200).json({
-      msg: `Casa Criada : ${Casa}`,
-    });
-
-    }catch(e){
-      return res.status(400).json({
-        msg : "Error"
-      })
-    }
-    
   }
 
   async index(req, res) {
@@ -50,43 +52,37 @@ class HouseController {
     });
   }
 
-
   async destroy(req, res) {
-    try{
-      const { house } = req.body;
-    const { user_id } = req.headers;
-    const casa = await Houses.findOne({ house });
-    
-
     try {
-      if (casa.user != user_id) {
-        res.status(401).json({
-          msg: "Não autorizado",
-        });
-      }
-      if (casa == "") {
+      const { house } = req.body;
+      const { user_id } = req.headers;
+      const casa = await Houses.findOne({ house });
+      
+      if (casa == null) {
         return res.status(400).json({
           msg: "Não encontrado",
         });
       }
+      
+      if (casa.user != user_id) {
+        return res.status(401).json({
+          msg: "Não autorizado",
+        });
+      }
+      
+      
+
+      await Houses.findOneAndDelete({ house });
+
+      return res.status(301).json({
+        msg: "Casa retirada",
+        "Casa Retirada": casa,
+      });
     } catch (e) {
-      res.status(401).json({
-        msg: `Error:`,
+      return res.status(400).json({
+        msg: e.message,
       });
     }
-
-    await Houses.deleteOne({ house });
-    return res.status(301).json({
-      msg: "Casa retirada",
-      "Casa Retirada": casa,
-    });
-      
-    }catch(e){
-      return res.status(400).json({
-        msg : "Error"
-      })
-    }
-    
   }
 }
 
--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -4,12 +4,8 @@ import User from "../models/User"
 class UserController{
     async store(req,res){
         const {idade, email} = req.body
-        
-
         const user = await User.create({email : email, idade : idade})
-        if(User.find(email == email)){
-            console.log('entrou')
-        }
+        
         return res.json({
             "Usuario Criado" : user
         })
